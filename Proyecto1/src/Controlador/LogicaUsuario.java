0a1db9ded39e30997a4d30c641cf8f36e6703d69
camila220 <64565852+camila220@users.noreply.github.com>
2020-04-29 21:32:24 -0500

    Update LogicaUsuario.java

--- Proyecto1/src/Controlador/LogicaUsuario.java
+++ Proyecto1/src/Controlador/LogicaUsuario.java
@@ -1,173 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Controlador;
+package CONTROLADOR;
 
-import Modelo.Conexion;
-import Modelo.Usuario;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import com.mysql.jdbc.Statement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableColumn;
+import VISTA.CajasMensaje;
+import VISTA.Juego;
+import VISTA.menu;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+import javax.swing.event.AncestorListener;
 
 /**
  *
- * @author walter
+ * @author Tutorias
  */
-public class LogicaUsuario {
-
-    ArrayList<Usuario> ListaUsuarios = new ArrayList<>();
-    private Connection conexion = null;
-    private PreparedStatement sentencias;
-    private Statement stmt = null;
-    private ResultSet rs;
-    private Usuario usuario;
-    private DefaultTableModel modelo;
-    String[] titulos = {"Nombre", "Apellido(s)", "Dirección", "Correo", "Contacto", "usuario", "Password", "Cedula", "ID"};
-    String[] fila = new String[9];
-
-    public ArrayList<Usuario> obtenerListaUsuarios() {
-        return ListaUsuarios;
-    }
-
-    public Usuario iniciaSesion(String user, String pass) {
-        try {
-            conexion = Conexion.getConnection();
-            sentencias = conexion.prepareStatement("select * from usuarios where  Usuario= ? and Pass= ?");
-            sentencias.setString(1, user);
-            sentencias.setString(2, pass);
-            rs = sentencias.executeQuery(); // ejecuto en la base de datos
-
-            if (rs != null) {
-                while (rs.next()) {
-                    usuario = new Usuario();
-                    usuario.setIdUsario(rs.getInt(1));
-                    usuario.setNombre(rs.getString(2));
-                    usuario.setPrimerApellido(rs.getString(3));
-                    usuario.setSedundoApellido(rs.getString(4));
-                    usuario.setContrasena(rs.getString(5));
-                    usuario.setCorreo(rs.getString(6));
-                    usuario.setDireccion(rs.getString(7));
-                    usuario.setNickname(rs.getString(8));
-                    usuario.setContacto(rs.getString(9));
-                    usuario.setCedula(rs.getString(10));
-                    usuario.setTipoUsuario(rs.getString(11));
-                    ListaUsuarios.add(usuario); // en mi lista queda cargada toda la informacion
-                }
-            }
-
-            return usuario;
-
-        } catch (Exception e) {
-            System.out.println("error en la consulta de la base de datos: " + e.getMessage());
-        }
-        return null;
-    }
-
-    public void crearUsuario(String nombre, String apellido, String cedula, String correo, String numeroContacto, String apellid2, String pass, String usuario, String direccion, String tipoUsuario) throws SQLException {
-        try {
-            conexion = Conexion.getConnection();
-            stmt = (Statement) conexion.createStatement();
-            if (conexion != null) {
-                stmt.executeUpdate("INSERT INTO usuarios(Nombre,PrimerApellido,SegundoApellido,Pass,Correo,Direccion,Usuario,NumeroContacto,Cedula,tipoUsuario) values(" + "'" + nombre + "','" + apellido + "','"
-                        + apellid2 + "','" + pass + "','" + correo + "','" + direccion + "','" + usuario + "','" + numeroContacto + "','"
-                        + cedula + "','" + tipoUsuario + "')");
-                System.out.println("Registro exitoso");
-            } else {
-                System.out.println("conexion fallida");
-            }
-
-        } catch (SQLException e) {
-            System.out.println("error en la consulta de la base de datos: " + e.getMessage());
-        } catch (Exception e) {
-            System.out.println("error en la consulta de la base de datos: " + e.getMessage());
-        }
-        finally{
-            sentencias=null;
-            conexion.close();
-        }
-    }
-
-    public void consultaTabla(JTable tabla, String tipoUsuario) {
-        try {
-            conexion = Conexion.getConnection();
-            stmt = (Statement) conexion.createStatement();
-            if (conexion != null) //System.out.println("conexion exitosa");
-            {
-                modelo = new DefaultTableModel(null, titulos);
-            }
-            ResultSet rs = stmt.executeQuery("select * from Usuarios");
-
-            while (rs.next()) {
-                if (tipoUsuario.equals(rs.getString("tipoUsuario"))) {
-                    fila[0] = rs.getString("Nombre");
-                    fila[1] = rs.getString("PrimerApellido");
-                    fila[2] = rs.getString("Direccion");
-                    fila[3] = rs.getString("Correo");
-                    fila[4] = rs.getString("NumeroContacto");
-                    fila[5] = rs.getString("Usuario");
-                    fila[6] = rs.getString("Pass");
-                    fila[7] = rs.getString("Cedula");
-                    fila[8] = rs.getString("Id");
-
-                    modelo.addRow(fila);
-
-                }
-            }
-            tabla.setModel(modelo);
-
-        } catch (Exception e) {
-            System.out.println("Error al extraer los datos de las tabals" + e.getMessage());
-        }
-
-    }
-
-    public void actualizar(String nombre, String apellido, String direccion, String correo, String numeroContacto, String user, String pass, String cedula, String id) throws SQLException {
-        try {
-            conexion = Conexion.getConnection();
-            sentencias = conexion.prepareStatement("UPDATE usuarios SET Nombre= "+"'"+nombre+"', "
-                + "PrimerApellido= "+"'"+apellido+"', "
-                + "Direccion= "+"'"+direccion+"', "
-                + "Correo= "+"'"+correo+"', "
-                + "NumeroContacto= "+"'"+numeroContacto+"', "
-                + "Usuario= "+"'"+user+"', "
-                + "Pass= "+"'"+pass+"', "
-                + "Cedula= "+"'"+cedula+"'"
-                + " where Id= " + id);
-            if (conexion != null) 
-                System.out.println("conexion exitosa");
-            
-            sentencias.executeUpdate();
-            System.out.println("exito");
+public class Control implements ActionListener {
     
-
-        } catch (Exception e) {
-            System.out.println("Error al extraer los datos de las tabals" + e.getMessage());
-        }
-        finally{
-            sentencias=null;
-            conexion.close();
-        }
-
-    }
-    public void eliminar(String user) {
-        try {
-            conexion = Conexion.getConnection();
-            sentencias = conexion.prepareStatement("DELETE FROM usuarios WHERE Id= "+user);
-
-            sentencias.executeUpdate();
-            System.out.println("Exito");
-        } catch (Exception e) {
-            System.out.println("Error "+e.getMessage());
-        }
+    private menu Menu;
+   Juego game = new Juego();
+    boolean Pausa = true;
+    CajasMensaje msj;
+     String NombreJugador;  
+    public Control()
+    {
+        Menu = new menu();
         
+        Menu.getBoton_ini().addActionListener(this);
+         
     }
-
+    
+    
+    
+    public  void iniciar() {
+        this.Menu.setVisible(true);
+        while(Pausa)  
+        {
+            System.out.println("Juego iniciado");     
+        }
+        game.arranque();
+       
+    }
+ 
+    
+    public static void main(String[] args) {
+        Control control = new Control();
+        control.iniciar();
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource().equals(Menu.getBoton_ini()))
+        {
+            NombreJugador = Menu.getTxt_NombreJugador().getText();
+            
+            Menu.setVisible(false);
+          Pausa = false;
+        }
+    }
 }
